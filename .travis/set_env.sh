15daa7bac908350f0b1edc2f715d9d9c777bf8d4
Ben Steadman <steadmanben1@gmail.com>
2020-04-03 15:22:00 +0100

    Account for 'v' prefix in Git release tags
    To be consistent with upstream, Git release tags should be prefixed with
    'v' e.g. 'v5.4.1' and Docker images should be tagged without the prefix
    e.g. unipartdigital/kafka-connect-jdbc:5.4.1. This commit strips the 'v'
    from the Git tag when pushing Docker containers via Travis.

--- .travis/set_env.sh
+++ .travis/set_env.sh
@@ -7,6 +7,7 @@ export DOCKERHUB_REPO="$TRAVIS_REPO_SLUG"
 if [[ -z "$TRAVIS_TAG" ]]; then
   export JAR_VERSION="$TRAVIS_BRANCH"
 else
-  export JAR_VERSION="$TRAVIS_TAG"
+  # Strip leading 'v' from Git tag e.g. v5.4.1 -> 5.4.1
+  export JAR_VERSION=${TRAVIS_TAG#v}
 fi
 
